27f012491bff04e11bfbe0399bb0de72de1cd3d8
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:32:13 -0400

    Add ticking support.

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /*
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 11:31:08
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:31:55
+*/
+
+'use strict';
+
+RoomObject.prototype.tick = function() {
+  return true; // No need to tick every last little thing.
+  // body...
+};
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 05:53:53
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
 * @Last Modified time: 2016-06-26 11:25:22
---
+++ src/RoomObject.js
@@ -0,0 +1,13 @@
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 11:31:08
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:31:55
+*/
+
+'use strict';
+
+RoomObject.prototype.tick = function() {
+  return true; // No need to tick every last little thing.
+  // body...
+};
