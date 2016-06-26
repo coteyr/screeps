5eff90274b318e90f163594fb648f84d6c322f6e
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:45:17 -0400

    add logging support

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 /*
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 11:40:25
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:45:04
+*/
+
+'use strict';
+
+var Log = {
+  info: function(message) {
+    if (logLevel >= 4) {
+      console.log(message);
+    }
+  },
+
+  warn: function(message) {
+    if (logLevel >= 3) {
+      console.log(message);
+    }
+  },
+  error: function(message) {
+    if (logLevel >= 2) {
+      console.log(message);
+    }
+  },
+
+  critical: function(message) {
+    if (logLevel >= 1) {
+      console.log(message);
+    }
+  }
+}
+module.exports = Log;
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 11:39:12
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
 * @Last Modified time: 2016-06-26 11:39:26
