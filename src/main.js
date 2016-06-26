66f31ecd4cc916d4e21f1c545f6fff02abebb1db
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:35:55 -0400

    Tick a room

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 06:00:56
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-* @Last Modified time: 2016-06-26 06:01:41
+* @Last Modified time: 2016-06-26 11:35:31
 */
 
 'use strict';
 
 module.exports.loop = function () {
+    Game.rooms.forEach(function(room) {
+      room.tick();
+    })
     //new Spawner(Game.spawns.Spawn1).tick();
-    Game.spawns.Spawn1.tick();
     /*_.filter(Game.creeps).forEach(function(creep) {
       creep.tick();
     });*/
