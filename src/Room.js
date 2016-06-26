252770671bef43ce040c73a7347e242624f72586
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:47:10 -0400

    use logger

--- src/Room.js
+++ src/Room.js
@@ -2,11 +2,12 @@
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 11:39:12
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-* @Last Modified time: 2016-06-26 11:39:26
+* @Last Modified time: 2016-06-26 11:46:47
 */
 
 'use strict';
 
 Room.prototype.tick = function() {
+  Log.info('Ticking Room: ' + this.name);
   return true;
 };
