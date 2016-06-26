538678d73609d2fd5b2181b07f106dc3f5b80f0c
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:38:39 -0400

    Tick rooms

--- main.js
+++ main.js
@@ -23,15 +23,15 @@ RoomObject.prototype.tick = function() {
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 06:00:56
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-* @Last Modified time: 2016-06-26 11:35:31
+* @Last Modified time: 2016-06-26 11:38:21
 */
 
 'use strict';
 
 module.exports.loop = function () {
-    Game.rooms.forEach(function(room) {
-      room.tick();
-    })
+  Object.keys(Game.rooms).forEach(function(key, index) {
+    this[key].tick();
+  }, Game.rooms);
     //new Spawner(Game.spawns.Spawn1).tick();
     /*_.filter(Game.creeps).forEach(function(creep) {
       creep.tick();
