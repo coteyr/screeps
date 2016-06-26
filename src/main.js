ba55969391b4ee57ebbab1f11fe408f65ce72cb0
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:46:22 -0400

    Log level set

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 06:00:56
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-* @Last Modified time: 2016-06-26 11:38:21
+* @Last Modified time: 2016-06-26 11:46:06
 */
 
 'use strict';
+
+var LogLevel = 4; //show it all
 
 module.exports.loop = function () {
   Object.keys(Game.rooms).forEach(function(key, index) {
