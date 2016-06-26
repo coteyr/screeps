999c6327f5c59bbf21362f4d6448e8a8620d94b9
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:27:01 -0400

    clean up stuff

---
+++ src/StructureSpawn.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 05:53:53
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:25:22
+*/
+
+'use strict';
