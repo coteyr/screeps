1c140edfa35728498eb19cac5decaef6e1ff55d7
Robert Cotey <coteyr@coteyr.net>
2016-06-26 11:39:41 -0400

    tick a room

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 /*
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 11:39:12
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:39:26
+*/
+
+'use strict';
+
+Room.prototype.tick = function() {
+  return true;
+};
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2016-06-26 11:31:08
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
 * @Last Modified time: 2016-06-26 11:31:55
---
+++ src/Room.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-06-26 11:39:12
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-06-26 11:39:26
+*/
+
+'use strict';
+
+Room.prototype.tick = function() {
+  return true;
+};
