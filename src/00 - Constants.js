7e75b316ce88b3fa32fc02bc24a402fe6c54dd87
Robert Cotey <coteyr@coteyr.net>
2016-08-26 22:31:22 -0400

    Code Cleaning

---
+++ src/00 - Constants.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Robert D. Cotey II <coteyr@coteyr.net>
+* @Date:   2016-08-26 22:31:08
+* @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
+* @Last Modified time: 2016-08-26 22:31:14
+*/
+
+'use strict';
