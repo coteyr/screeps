85981626ce69e7f73e5b7850405519910024bb59
Robert Cotey <coteyr@coteyr.net>
2017-03-06 22:17:33 -0500

    Add visuilizer class more cleanup

--- src/lib/Global.js
+++ src/lib/Global.js
@@ -2,7 +2,7 @@
 * @Author: Robert D. Cotey II <coteyr@coteyr.net>
 * @Date:   2017-02-19 13:50:37
 * @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-* @Last Modified time: 2017-03-02 00:12:02
+* @Last Modified time: 2017-03-06 21:08:24
 */
 
 'use strict';
@@ -24,24 +24,15 @@ global.addRamps = function(roomName) {
 }
 global.showWalls = function(roomName) {
   let room = Game.rooms[roomName]
-  _.each(Storage.read(room.name + '-wall-spots', []), s => {
-    room.visual.rect(s.x - 0.5, s.y - 0.5, 1, 1, { fill: Config.colors.purple })
-  })
+  Visualizer.showSquares(roomName, Storage.read(room.name + '-wall-spots', []), Config.colors.purple)
 }
 global.showRamps = function(roomName) {
   let room = Game.rooms[roomName]
-  _.each(Storage.read(room.name + '-ramp-spots', []), r => {
-    room.visual.rect(r.x - 0.5, r.y - 0.5, 1, 1, { fill: Config.colors.green })
-  })
+  Visualizer.showSquares(roomName, Storage.read(room.name + '-ramp-spots', []), Config.colors.green)
 }
 global.showExtensions = function(roomName) {
   let room = Game.rooms[roomName]
-  let i = 1
-  _.each(Storage.read(room.name + '-extension-spots', []), e => {
-    room.visual.circle(e.x, e.y, { fill: Config.colors.red })
-    room.visual.text(i, e.x, e.y, {color: Config.colors.green, font: 0.25 })
-    i = i + 1
-  })
+  Visualizer.showCircles(roomName, Storage.read(room.name + '-extension-spots', []), Config.colors.red, true)
 }
 global.pruneWalls = function(roomName) {
   RoomBuilder.pruneWalls(roomName)
@@ -53,3 +44,4 @@ global.clearBuiltWalls = function(roomName) {
 global.planExtensions = function(roomName) {
   RoomBuilder.buildOutExtensions(roomName)
 }
+
